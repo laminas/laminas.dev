2aee5bac9f0380b09ea3209128271e4f440d6a43
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-03-04 10:28:14 -0600

    qa: test behaviors of WebAPIMessage

---
+++ test/Slack/Domain/WebAPIMessageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppTest\Slack\Domain;
+
+use App\Slack\Domain\Message;
+use App\Slack\Domain\WebAPIMessage;
+use Assert\AssertionFailedException;
+use PHPUnit\Framework\TestCase;
+
+class WebAPIMessageTest extends TestCase
+{
+    public function testExtendsMessageClass(): void
+    {
+        $message = new WebAPIMessage();
+        $this->assertInstanceOf(Message::class, $message);
+    }
+
+    public function testInvalidatesMessageIfChannelIsEmpty(): void
+    {
+        $message = new WebAPIMessage();
+        $message->setText('message text');
+
+        $this->expectException(AssertionFailedException::class);
+        $this->expectExceptionMessageRegExp('/^(?!Text must be non-empty)/');
+        $message->validate();
+    }
+
+    public function testRendersChannelInMessagePayload(): void
+    {
+        $message = new WebAPIMessage();
+        $message->setText('message text');
+        $message->setChannel('github');
+        
+        $this->assertSame([
+            'channel' => '#github',
+            'text'    => 'message text',
+        ], $message->toArray());
+    }
+}
