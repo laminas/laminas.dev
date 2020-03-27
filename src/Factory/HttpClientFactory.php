98e5b22b445f04700793e61e48d963bc9f85e53c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2020-03-27 11:12:06 -0500

    fix: do not use http_errors middleware with Guzzle
    I had been operating on the assumption that Guzzle always returned a
    response. However, by default, it registers middleware that raises an
    exception when a 400 or greater response code is seen.
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/Factory/HttpClientFactory.php
+++ src/Factory/HttpClientFactory.php
@@ -6,15 +6,24 @@ namespace App\Factory;
 
 use App\HttpClient;
 use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+
+use function GuzzleHttp\choose_handler;
 
 class HttpClientFactory
 {
     public function __invoke(ContainerInterface $container): HttpClient
     {
+        $stack = new HandlerStack();
+        $stack->setHandler(choose_handler());
+        $stack->push(Middleware::redirect(), 'allow_redirects');
+        $stack->push(Middleware::prepareBody(), 'prepare_body');
+
         return new HttpClient(
-            new Guzzle(),
+            new Guzzle(['handler' => $stack]),
             $container->get(RequestFactoryInterface::class)
         );
     }
