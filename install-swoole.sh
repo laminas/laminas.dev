f42d7ad41d0e16ca9b33af81a09db6f1668b6c89
Matthew Weier O'Phinney <matthew@weierophinney.net>
2022-07-27 14:41:21 -0500

    fix: combine offical script and updates that include valgrind
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- install-swoole.sh
+++ install-swoole.sh
@@ -6,63 +6,105 @@ run() {
     # Run the compilation process.
     cd "${PLATFORM_CACHE_DIR}" || exit 1;
 
-    if [ ! -f "${PLATFORM_CACHE_DIR}/swoole/modules/swoole.so" ]; then
-        ensure_source
-        checkout_version "$1"
-        compile_source
+    local swoole_version="$1"
+    local swoole_type="$2"
+
+    local php_version
+    php_version=$(php -r "echo php_version;")
+
+    local version_prefix="v${swoole_version}-php${php_version}"
+    version_prefix="${version_prefix//\./_}"
+
+    if [ ! -f "${PLATFORM_CACHE_DIR}/${swoole_type}/swoole-src/modules/${version_prefix}-${swoole_type}.so" ]; then
+        ensure_source "$swoole_type"
+        checkout_version "${PLATFORM_CACHE_DIR}/${swoole_type}/swoole-src" "v${swoole_version}"
+        compile_source "${PLATFORM_CACHE_DIR}/${swoole_type}"
+        move_extension "$swoole_type" "$version_prefix"
     fi
 
-    copy_lib
-    enable_lib
+    copy_lib "$2" "$version_prefix"
+    enable_lib "$2"
 }
 
 enable_lib() {
     # Tell PHP to enable the extension.
-    echo "Enabling Swoole extension."
-    echo "extension=${PLATFORM_APP_DIR}/swoole.so" >> "${PLATFORM_APP_DIR}/php.ini"
+    local swoole_type="$1"
+
+    echo "---------------------------------"
+    echo "Enabling ${swoole_type} extension."
+    echo -e "\nextension=${PLATFORM_APP_DIR}/${swoole_type}.so" >> "${PLATFORM_APP_DIR}/php.ini"
 }
 
 copy_lib() {
     # Copy the compiled library to the application directory.
-    echo "Installing Swoole extension."
-    cp "${PLATFORM_CACHE_DIR}/swoole/modules/swoole.so" "${PLATFORM_APP_DIR}"
+    local swoole_type="$1"
+    local version_prefix="$2"
+
+    echo "---------------------------------"
+    echo "Installing ${swoole_type} extension."
+    cp "${PLATFORM_CACHE_DIR}/${swoole_type}/swoole-src/modules/${version_prefix}-${swoole_type}.so" "${PLATFORM_APP_DIR}/${swoole_type}.so"
 }
 
 checkout_version () {
+    local checkout_dir="$1"
+    local version="$2"
     # Check out the specific Git tag that we want to build.
-    git checkout "$1"
+    cd "$checkout_dir"
+    git checkout "$version"
 }
 
 ensure_source() {
     # Ensure that the extension source code is available and up to date.
-    if [ -d "swoole" ]; then
-        cd swoole || exit 1;
+    local swoole_type="$1"
+
+    mkdir -p "${PLATFORM_CACHE_DIR}/${swoole_type}"
+    cd "${PLATFORM_CACHE_DIR}/${swoole_type}"
+
+    if [ -d "swoole-src" ]; then
+        cd swoole-src || exit 1;
         git fetch --all --prune
     else
-        git clone https://github.com/swoole/swoole-src.git swoole
-        cd swoole || exit 1;
+        git clone "https://github.com/${swoole_type}/swoole-src.git"
+        cd swoole-src || exit 1;
     fi
 
+    cd "${PLATFORM_CACHE_DIR}/${swoole_type}";
+
     if [ -d "valgrind" ]; then
-        cd valgrind || exit 1;
+        cd valgrind || exit 1
         git fetch --all --prune
     else
         git clone git://sourceware.org/git/valgrind.git valgrind
-        cd valgrind || exit 1;
+        cd valgrind || exit 1
     fi
 }
 
 compile_source() {
     # Compile the extension.
-    cd valgrind
+    local base_dir="$1"
+
+    echo "Compiling valgrind"
+    cd "${base_dir}/valgrind"
     ./autogen.sh
-    ./configure --prefix="${PLATFORM_CACHE_DIR}/swoole"
+    ./configure --prefix="${base_dir}/swoole-src"
     make
     make install
-    cd ..
+
+    echo "Compiling (Open)Swoole"
+    cd "${base_dir}/swoole-src"
+    phpize clean
     phpize
     ./configure
     make
+}
+
+move_extension() {
+    local swoole_type="$1"
+    local version_prefix="$2"
+
+    echo "---------------------------------"
+    echo "Moving built extension to identified folder."
+    mv "${PLATFORM_CACHE_DIR}/${swoole_type}/swoole-src/modules/${swoole_type}.so" "${PLATFORM_CACHE_DIR}/${swoole_type}/swoole-src/modules/${version_prefix}-${swoole_type}.so"
 }
 
 ensure_environment() {
@@ -74,13 +116,28 @@ ensure_environment() {
 }
 
 ensure_arguments() {
-    # If no version was specified, don't try to guess.
+    # If no Swoole repository was specified, don't try to guess.
     if [ -z "$1" ]; then
-        echo "No version of the (open)swoole extension specified.  You must specify a tagged version on the command line."
+        echo "No version of the Swoole project specified. (swoole/openswoole)."
+        exit 1;
+    fi
+
+    if [[ ! "$1" =~ ^(swoole|openswoole)$ ]]; then
+        echo "The requested Swoole project is not supported: ${1}"
+        echo "Aborting."
+        exit 1;
+    fi
+
+    # If no version was specified, don't try to guess.
+    if [ -z "$2" ]; then
+        echo "No version of the ${1} extension specified.  You must specify a tagged version on the command line."
         exit 1;
     fi
 }
 
 ensure_environment
-ensure_arguments "$1"
-run "$1"
+ensure_arguments "$1" "$2"
+
+SWOOLE_PROJECT=$1;
+SWOOLE_VERSION=$(sed "s/^[=v]*//i" <<< "$2" | tr '[:upper:]' '[:lower:]')
+run "$SWOOLE_VERSION" "$SWOOLE_PROJECT"
